7df156cdd0a94185f5d8ed34d23f37857ffa5e78
wsw001 <wsw001@126.com>
2018-10-08 13:50:24 +0800

    the third change

--- textprobarv1.py
+++ textprobarv1.py
@@ -3,15 +3,15 @@ import time
 
 def textbar(modeNo):
     x=0.1
-    str1=' x'
-    str2=' x+(1-math.sin(x*math.pi*2+math.pi/2)/-8)'
-    str3=' x+(1-math.sin(x*math.pi*2+math.pi/2)/8)'
-    str4=' x + math.sin(x * math.pi * 5) / 20'
-    str5=' x + math.sin(x * math.pi * 20) / 80'
-    str6=' (x + (1 - x) * 0.03)**2'
-    str7= ' 1 + (1 - x)**2'
-    str8=' (x + (1 - x) / 2) ** 8'
-    str9=' 1 + (1 - x)**(3)'
+    str1='x'
+    str2='x+(1-math.sin(x*math.pi*2+math.pi/2)/-8)'
+    str3='x+(1-math.sin(x*math.pi*2+math.pi/2)/8)'
+    str4='x + math.sin(x * math.pi * 5) / 20'
+    str5='x + math.sin(x * math.pi * 20) / 80'
+    str6='(x + (1 - x) * 0.03)**2'
+    str7='1 + (1 - x)**2'
+    str8='(x + (1 - x) / 2) ** 8'
+    str9='1 + (1 - x)**(3)'
     wd=100
     print("Begin".center(105,'='))
     start=time.perf_counter()
@@ -22,7 +22,7 @@ def textbar(modeNo):
         end=time.perf_counter()
         stu=end-start
         x=eval(eval(choice))
-        print("\r{0}%[{1}{2}]{3:.2f}s -{4:.2f}".format(i,a,b,stu,x),end='')
+        print("\r{0}%[{1}{2}]{3:.2f}s".format(i,a,b,stu),end='')
         time.sleep(x/10)
     print("\n"+"END".center(105,'='))
     print("\n")
